095292d32808df53ab8eb2ffc1125c3a6264ca11
oneul0 <gihoon1011@gmail.com>
2024-11-01 22:09:00 +0900

    [PGS] 하노이의탑 / L2 / 48m / 김기훈

--- Week02/PGS_하노이의 탑/hoon.java
+++ Week02/PGS_하노이의 탑/hoon.java
@@ -1,1 +1,22 @@
+import java.util.*;
+class Solution {
+    ArrayList<int[]> tmp = new ArrayList<>();
+    public int[][] solution(int n) {
+        dfs(n, 1, 2, 3);
+        int[][] answer = new int[tmp.size()][];
+        for(int i = 0; i<tmp.size(); i++){
+            answer[i] = tmp.get(i);
+        }
+        return answer;
+    }
 
+    void dfs(int n, int from, int by, int to){
+        //기저사례
+        if(n == 0) return;
+
+        dfs(n-1, from, to, by);
+        tmp.add(new int[]{from, to});
+        dfs(n-1, by, from, to);
+
+    }
+}
