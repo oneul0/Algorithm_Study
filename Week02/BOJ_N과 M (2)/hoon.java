b438ef71e6c7278c73f63642ea7f8f7c85ccc926
oneul0 <gihoon1011@gmail.com>
2024-11-04 11:19:57 +0900

    [BOJ] N과 M 1,2 / S3 / 30m, 40m / 김기훈

--- Week02/BOJ_N과 M (2)/hoon.java
+++ Week02/BOJ_N과 M (2)/hoon.java
@@ -1,1 +1,34 @@
+import java.io.*;
 
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n,m;
+    static boolean[] chk = new boolean[9];
+    static int[] arr = new int[9];
+    public static void main(String[] args) throws IOException {
+        String[] nm = br.readLine().split(" ");
+        n = Integer.parseInt(nm[0]);
+        m = Integer.parseInt(nm[1]);
+
+        bt(0,1);
+    }
+    //idx는 자릿수 num은 시작수(num~n까지 방문하지 않았던 숫자를 추가하기 위함)
+    static void bt(int idx, int num){
+        if(idx == m){
+            for(int i = 0; i<m; i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i = num; i<=n; i++){
+            if(!chk[i]){
+                chk[i] = true;
+                arr[idx] = i;
+                bt(idx+1, i+1);
+                chk[i] = false;
+            }
+        }
+    }
+}
