826033ce05e1b1b2187b7ff46b1f17dab96a18ae
oneul0 <gihoon1011@gmail.com>
2024-11-05 14:00:18 +0900

    [BOJ] N-Queen / G4 / 30m / 김기훈

--- Week02/BOJ_N-Queen/hoon.java
+++ Week02/BOJ_N-Queen/hoon.java
@@ -1,1 +1,41 @@
+import java.io.*;
 
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int ans = 0, N;
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        bt(0, new int[N]);
+        bw.write(ans+"");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void bt(int col, int[] row){
+        if(col == N){
+            ans++;
+            return;
+        }
+
+        //row 탐색 loop
+        for(int i = 0; i<N; i++){
+            boolean is_safe = true;
+            //이전까지의 row 탐색 loop
+            for(int j = 0; j <col; j++){
+                //이전에 놓았던 row와 같거나 || 기울기가 같은 이전의 놓았던 인덱스가 있으면 pass
+                if(row[j] == i || Math.abs(row[j]-i) == col- j) {
+                    is_safe = false;
+                    break;
+                }
+            }
+            if(is_safe){
+                row[col] = i;
+                bt(col+1, row);
+            }
+        }
+
+    }
+}
