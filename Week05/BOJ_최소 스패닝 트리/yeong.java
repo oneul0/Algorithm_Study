61c81cac24d1470c653b815379b705db85155af6
eden <feijoaplz@gmail.com>
2024-11-28 16:09:35 +0900

    [PGS] 최소 스패닝 트리 / G4 / 3h / 장은영

--- Week05/BOJ_최소 스패닝 트리/yeong.java
+++ Week05/BOJ_최소 스패닝 트리/yeong.java
@@ -2,70 +2,80 @@ import java.io.*;
 import java.util.*;
 
 //프림 알고리즘
-public class Main {
-    static int total;
-    static List<Node>[] list;
-    static boolean[] visited;
-    static class Node implements Comparable<Node>{
-        int to;
-        int value;
+// 정점 정보 클래스
+class Node implements Comparable<Node>{
+    int to;
+    int weight;
 
-        public Node(int to, int value) {
-            this.to = to;
-            this.value = value;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.value - o.value;
-        }
+    public Node(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
     }
+
+    // 우선 순위 큐 활용
+    @Override
+    public int compareTo(Node node) {
+        return weight - node.weight;
+    }
+}
+
+public class Main {
+    static StringTokenizer st;
+    static int v; // 정점의 개수
+    static int e; // 간선의 개수
+    static boolean[] visit; // 정점 방문 여부
+    static PriorityQueue<Node> queue; // 우선순위 큐
+    static ArrayList<ArrayList<Node>> nodeList; // 정점 정보 저장
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int v = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
 
-        list = new ArrayList[v+1];
-        visited = new boolean[v+1];
-        for(int i=1; i<v+1; i++) {
-            list[i] = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        visit = new boolean[v + 1];
+        queue = new PriorityQueue<>();
+
+        nodeList = new ArrayList<>();
+        for (int i = 0; i <= v; i++) {
+            nodeList.add(new ArrayList<>());
         }
 
-        for(int i=0; i<e; i++) {
+        // 간선 정보 입력 (정점 연결리스트)
+        for(int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            list[a].add(new Node(b,w));
-            list[b].add(new Node(a,w));
+
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            nodeList.get(v1).add(new Node(v2,weight));
+            nodeList.get(v2).add(new Node(v1,weight));
         }
 
-        prim(1);
-        System.out.println(total);
-    }
+        queue.offer(new Node(1,0));
 
-    static void prim(int start) {
-        Queue<Node> pq = new PriorityQueue<>();
+        int sum = 0;
+        while(!queue.isEmpty()){
+            Node n = queue.poll();
+            int to = n.to;
+            int weight = n.weight;
 
-        pq.add(new Node(start,0));
-        while(!pq.isEmpty()) {
-            Node p = pq.poll();
-            int node = p.to;
-            int weight = p.value;
+            // 이미 방문한 정점이라면 탐색 X
+            if(visit[to]) {
+                continue;
+            }
+            visit[to] = true;
+            sum += weight;
 
-            if(visited[node]) continue;
-            // 선택한 간선의 정점으로부터 가장 낮은 가중치 갖는 정점 선택
-            visited[node]= true;
-            total += weight;
-
-            for(Node next : list[node]) {
-                if(!visited[next.to]) {
-                    pq.add(next);
+            // 연결되어 있는 방문하지 않은 정점 큐에 추가
+            for(Node next : nodeList.get(to)) {
+                if(!visit[next.to]) {
+                    queue.offer(next);
                 }
             }
         }
 
+        System.out.println(sum);
     }
 }
