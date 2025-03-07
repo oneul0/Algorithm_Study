c49a59e073acbd833249c01191652231d33aa18f
kUZEEwon <pink7519@pusan.ac.kr>
2025-03-07 21:40:25 +0900

    [BOJ] 디스크 트리/ G3/ 2H/ 구지원 [BOJ] 전력난/ G4/ 1H/ 구지원 [BOJ] 개미굴/ G3/ 90m/ 구지원

---
+++ Week18/BOJ_디스크트리/won.java
@@ -0,0 +1,53 @@
+package BOJ_디스크트리;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class TrieNode{
+        Map<String, TrieNode> childNode = new HashMap<>();
+
+        TrieNode(){
+            /* no-op */
+        }
+
+        public void insert(String strs) {
+            TrieNode trieNode = this;
+            String[] str = strs.split("\\\\");
+            for(String s : str) {
+                trieNode.childNode.putIfAbsent(s, new TrieNode());
+                trieNode = trieNode.childNode.get(s);
+            }
+        }
+
+        public void print(TrieNode cur, int depth) {
+            TrieNode trieNode = cur;
+            if(trieNode.childNode !=null) {
+                List<String> list = new ArrayList<>(trieNode.childNode.keySet());
+                Collections.sort(list);
+                for(String str : list) {
+                    for(int i=0; i<depth; i++) {
+                        System.out.print(" ");
+                    }
+                    System.out.println(str);
+                    print(trieNode.childNode.get(str), depth+1);
+                }
+            }
+
+        }
+
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        TrieNode trie = new TrieNode();
+        for(int i=0; i<n; i++) {
+            String line = br.readLine();
+            trie.insert(line);
+        }
+
+        trie.print(trie, 0);
+
+    }
+}
