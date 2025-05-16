3284b5721c6af4223a2f0790e9ffdb61d59bafcc
051198Hz <peeper_o_o@naver.com>
2025-05-16 23:29:12 +0900

    [BOJ] 문제집 / G2 / 1H / 김윤회

---
+++ Week28/문제집/yune.swift
@@ -0,0 +1,155 @@
+import Foundation
+
+struct Heap<T> {
+    private var nodes = [T]()
+    
+    private var orderCriteria: (T, T) -> Bool
+    
+    public init(sort: @escaping (T, T) -> Bool) {
+        self.orderCriteria = sort
+    }
+    
+    public var count: Int {
+        return nodes.count
+    }
+    
+    public func peek() -> T? {
+        return nodes.first
+    }
+    
+    func isEmpty() -> Bool {
+        return nodes.isEmpty
+    }
+    
+    public mutating func insert(_ value: T) {
+        nodes.append(value)
+        shiftUp(nodes.count - 1)
+    }
+    
+    public mutating func remove() -> T? {
+        guard !nodes.isEmpty else { return nil }
+        
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        } else {
+            let value = nodes[0]
+            nodes[0] = nodes.removeLast()
+            shiftDown(0)
+            return value
+        }
+    }
+    
+    public mutating func remove(at index: Int) -> T? {
+        guard index < nodes.count else { return nil }
+        
+        let lastIndex = nodes.count-1
+        if index != lastIndex {
+            nodes.swapAt(index, lastIndex)
+            shiftDown(from: index, until: lastIndex)
+            shiftUp(index)
+        }
+        
+        return nodes.removeLast()
+    }
+    
+    private func parentIndex(ofIndex i: Int) -> Int {
+        return (i - 1) / 2
+    }
+    
+    private func leftChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 1
+    }
+    
+    private func rightChildIndex(ofIndex i: Int) -> Int {
+        return 2*i + 2
+    }
+    
+    private mutating func shiftUp(_ index: Int) {
+        var childIndex = index
+        let child = nodes[childIndex]
+        var parentIndex = self.parentIndex(ofIndex: index)
+        
+        while childIndex > 0 && orderCriteria(child, nodes[parentIndex]) {
+            nodes[childIndex] = nodes[parentIndex]
+            childIndex = parentIndex
+            parentIndex = self.parentIndex(ofIndex: childIndex)
+        }
+        
+        nodes[childIndex] = child
+    }
+    
+    private mutating func shiftDown(from index: Int, until endIndex: Int) {
+        let leftChildIndex = self.leftChildIndex(ofIndex: index)
+        let rightChildIndex = leftChildIndex + 1
+        
+        var first = index
+        if leftChildIndex < endIndex && orderCriteria(nodes[leftChildIndex], nodes[first]) {
+            first = leftChildIndex
+        }
+        if rightChildIndex < endIndex && orderCriteria(nodes[rightChildIndex], nodes[first]) {
+            first = rightChildIndex
+        }
+        if first == index { return }
+        
+        nodes.swapAt(index, first)
+        shiftDown(from: first, until: endIndex)
+    }
+    
+    private mutating func shiftDown(_ index: Int) {
+        shiftDown(from: index, until: nodes.count)
+    }
+    
+}
+
+class FileIO {
+    @inline(__always) private var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()) + [0], byteIdx = 0
+    
+    @inline(__always) private func readByte() -> UInt8 {
+        defer { byteIdx += 1 }
+        return buffer.withUnsafeBufferPointer { $0[byteIdx] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var number = 0, byte = readByte(), isNegative = false
+        while byte == 10 || byte == 32 { byte = readByte() }
+        if byte == 45 { byte = readByte(); isNegative = true }
+        while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
+        return number * (isNegative ? -1 : 1)
+    }
+}
+
+
+//첫째 줄에 문제의 수 N(1 ≤ N ≤ 32,000)과 먼저 푸는 것이 좋은 문제에 대한 정보의 개수 M(1 ≤ M ≤ 100,000)이 주어진다.
+//둘째 줄부터 M개의 줄에 걸쳐 두 정수의 순서쌍 A,B가 빈칸을 사이에 두고 주어진다. 이는 A번 문제는 B번 문제보다 먼저 푸는 것이 좋다는 의미이다.
+let io = FileIO()
+
+let n = io.readInt(),
+    m = io.readInt()
+
+var indegreeList = [Int](repeating: 0, count: n+1)
+var graph = [[Int]](repeating: [Int](), count: n+1)
+var answer = ""
+
+(0..<m).forEach { _ in
+    let a = io.readInt()
+    let b = io.readInt() //0부터 시작하는게 아닌, 1부터 시작하는 노드 중 하나
+    indegreeList[b] += 1
+    graph[a].append(b)
+}
+
+var heap = Heap<Int>(sort: <) // <; 오름차순, >; 내림차순
+
+for i in 1...n {
+    if indegreeList[i] == 0 { heap.insert(i) }
+}
+
+while !heap.isEmpty() {
+    let problem = heap.remove()!
+    answer.write("\(problem) ") // == answer.append("\(problem) ")
+    for g in graph[problem] {
+        indegreeList[g] -= 1
+        if indegreeList[g] == 0 { heap.insert(g) }
+    }
+}
+
+print(answer)
